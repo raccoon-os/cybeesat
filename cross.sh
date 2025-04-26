5b5dd1d2984bb4456c821033ede99622da11882d
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2025-04-26 12:32:19 +0200

    Remove unused files

--- cross.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-TARGET=armv7-unknown-linux-musleabihf
-INSTALL_DIR=install/usr
-
-mkdir -p install/usr
-sh -c "cross-util run --target $TARGET -- \"$* --locked --root $INSTALL_DIR --target $TARGET\""
